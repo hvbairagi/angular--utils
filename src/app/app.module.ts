4c3156c706f2d5641500941743943a33fbe585d3
hvbairagi <hvbairagi@gmail.com>
2021-03-01 17:18:34 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,25 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateComponent } from './template/template.component';
-import { ChildComponent } from './child/child.component';
-import { ParentComponent } from './parent/parent.component';
-import { Child2Component } from './child2/child2.component';
-import { ExcelsheetComponent } from './excelsheet/excelsheet.component';
-import { FileUploadAndRetrieveComponent } from './file-upload-and-retrieve/file-upload-and-retrieve.component';
+import { TemplateComponent } from './other components/template/template.component';
+import { ChildComponent } from './other components/child/child.component';
+import { ParentComponent } from './other components/parent/parent.component';
+import { Child2Component } from './other components/child2/child2.component';
+import { ExcelsheetComponent } from './file-components/excelsheet/excelsheet.component';
+import { FileUploadAndRetrieveComponent } from './file-components/file-upload-and-retrieve/file-upload-and-retrieve.component';
 import { FileUploadAndDownloadComponent } from './file-components/file-upload-and-download/file-upload-and-download.component';
 
 @NgModule({
-  declarations: [AppComponent, TemplateComponent, ChildComponent, ParentComponent, Child2Component, ExcelsheetComponent, FileUploadAndRetrieveComponent, FileUploadAndDownloadComponent],
+  declarations: [
+    AppComponent,
+    TemplateComponent,
+    ChildComponent,
+    ParentComponent,
+    Child2Component,
+    ExcelsheetComponent,
+    FileUploadAndRetrieveComponent,
+    FileUploadAndDownloadComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
